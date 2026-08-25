9ba1a8328a3444e2eeb25f04d41d31d274c33422
leodm5674 <98219987+leodm5674@users.noreply.github.com>
2026-08-24 21:52:01 -0300

    Create atividade1.java

---
+++ Vetores/atividades/atividade1.java
@@ -0,0 +1,23 @@
+
+public class atividade1 {
+    /*Crie um programa java que:
+
+-Considerando um vetor "vetA" com 4 posições do tipo int, solicite ao usuário um valor ser armazenado em cada posição.
+
+-Considerando um vetor "vetB" com 4 posições do tipo String, solicite ao usuário um valor ser armazenado em cada posição.
+
+-Imprima na tela o conteúdo armazenado em cada posição do vetor "vetB", repetindo-o a quantidade de vezes expressa na mesma posição do vetor "vetA".
+
+Ex: Se vetA[0] tiver o valor 4 e vetB[0] tiver a palavra "java", escreve "java" 4 vezes na tela*/
+ public static void main(String[] args) {
+    int tamanhovetor = 4;
+    int vetA[] =  new int[tamanhovetor];
+    String vetB[] = new String[tamanhovetor];
+    
+    System.out.println("Digite quatro numeros para vetA");
+    
+
+
+
+    }
+}
