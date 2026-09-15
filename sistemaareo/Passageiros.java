9c1750cd4074e4f84f74f1480e79c75080414ee7
Leandro Dierings de Morais <98219987+leodm5674@users.noreply.github.com>
2026-09-14 21:43:03 -0300

    Create Passageiros.java

---
+++ sistemaareo/Passageiros.java
@@ -0,0 +1,46 @@
+package atividade;
+
+public class Passageiros {
+    private String nome;
+    private int idade;
+    private char sexo;
+    private int cpf;
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public char getSexo() {
+        return this.sexo;
+    }
+
+    public int getCpf() {
+        return this.cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    
+
+
+
+
+    
+}
