6edcd54814defa994b2550c6135f25045d5beec2
Leandro Dierings de Morais <98219987+leodm5674@users.noreply.github.com>
2026-09-21 10:11:05 -0300

    Add properties and methods to Companhia class

--- sistemaareo/Companhia.java
+++ sistemaareo/Companhia.java
@@ -1,6 +1,30 @@
-package atividade;
-
 public class Companhia {
-    
+    private String nomeDaCompanhia;
+    private Voo[] listaDeVoos = new Voo[10];
+    private int totalDeVoos;
 
+    public String getNomeDaCompanhia() {
+        return nomeDaCompanhia;
+    }
+
+    public void setNomeDaCompanhia(String nomeDaCompanhia) {
+        this.nomeDaCompanhia = nomeDaCompanhia;
+    }
+
+    public Voo[] getListaDeVoos() {
+        return listaDeVoos;
+    }
+
+    public int getTotalDeVoos() {
+        return totalDeVoos;
+    }
+
+    public void cadastrarVoo(Voo voo) {
+        if (totalDeVoos < listaDeVoos.length) {
+            listaDeVoos[totalDeVoos] = voo;
+            totalDeVoos++;
+        } else {
+            System.out.println("Companhia lotada");
+        }
+    }
 }
